aa2aa715fe1689c25e5fedd22dcefb78d72888f0
TreeSparks <99034584+TreeSparks@users.noreply.github.com>
2022-12-19 15:49:28 -0500

    Update Art.py

--- Art.py
+++ Art.py
@@ -1,23 +1,30 @@
 import turtle
 
-t = turtle.Turtle()
-
-ANGLE = 4
+WOOD_COLOR = 'white'
+LEAVES_COLOR = 'green'
 BACKGROUND_COLOR = 'black'
-CIRCLE_COLOR = 'green'
 
+t = turtle.Turtle()
 t.screen.bgcolor(BACKGROUND_COLOR)
 t.pensize(2)
-
-t.speed(0)
+t.color('green')
 t.left(90)
-t.color(BACKGROUND_COLOR)
-t.forward(100)
-t.right(90)
-
-t.color(CIRCLE_COLOR)
-for i in range(0,int(360/ANGLE)):
-    t.forward(12)
-    t.right(ANGLE)
-
+t.backward(100)
+t.speed(0) # 1-10 (0 turns off animation and goes fast as possible)
+#t.shape('turtle')
+def tree(i):
+    if i<10:
+        return
+    else:
+        t.forward(i)
+        t.color(LEAVES_COLOR)
+        t.circle(2)
+        t.color(WOOD_COLOR)
+        t.left(30)
+        tree(3*i/4)
+        t.right(60)
+        tree(3*i/4)
+        t.left(30)
+        t.backward(i)
+tree(100)
 turtle.done()
