208d1b56646fa937b428c510c13506f66b32f62e
TreeSparks <99034584+TreeSparks@users.noreply.github.com>
2022-12-19 15:24:16 -0500

    Update Art.py

--- Art.py
+++ Art.py
@@ -1,13 +1,23 @@
-import turtle 
+import turtle
 
 t = turtle.Turtle()
-t.screen.bgcolor('black')
-t.color('purple')
-t.pensize(2)
-t.speed(0)
 
-for i in range(0,4):
-    t.forward(100)
-    t.right(90)
+ANGLE = 4
+BACKGROUND_COLOR = 'black'
+CIRCLE_COLOR = 'green'
+
+t.screen.bgcolor(BACKGROUND_COLOR)
+t.pensize(2)
+
+t.speed(0)
+t.left(90)
+t.color(BACKGROUND_COLOR)
+t.forward(100)
+t.right(90)
+
+t.color(CIRCLE_COLOR)
+for i in range(0,int(360/ANGLE)):
+    t.forward(12)
+    t.right(ANGLE)
 
 turtle.done()
