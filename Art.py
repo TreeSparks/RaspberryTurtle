4c5a5ec014cdf5a97e19726a7ec5ccf67ea9e944
TreeSparks <99034584+TreeSparks@users.noreply.github.com>
2022-12-19 01:35:18 -0500

    Add files via upload

---
+++ Art.py
@@ -0,0 +1,15 @@
+import turtle 
+
+t = turtle.Turtle()
+t.screen.bgcolor('black')
+t.color('purple')
+t.pensize(2)
+t.speed(0)
+
+for i in range(0,4):
+    t.forward(100)
+    t.right(90)
+
+
+
+turtle.done()
